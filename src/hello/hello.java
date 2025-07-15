4f29ada903165977b6eebf2f2a497642ac77c89e
ganeshcoder4043 <ganesha4043@gmail.com>
2025-07-16 00:19:21 +0530

    test run

--- src/hello/hello.java
+++ src/hello/hello.java
@@ -2,6 +2,6 @@ package hello;
 
 public class hello {
 	public static void main(String[] args) {
-		System.out.println("this is hello world");
+		System.out.println("this is ffdnkabvfjbvfhello world");
 	}
 }
