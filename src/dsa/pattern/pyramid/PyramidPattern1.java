d24522d365983eb49f246d9dd2f6da2f59dec2b3
ganeshcoder4043 <ganesha4043@gmail.com>
2025-07-16 00:26:37 +0530

    test class & packages

---
+++ src/dsa/pattern/pyramid/PyramidPattern1.java
@@ -0,0 +1,7 @@
+package dsa.pattern.pyramid;
+
+public class PyramidPattern1 {
+	public static void main(String[] args) {
+		System.out.println("this is PyramidPattern1");
+	}
+}
---
+++ src/dsa/pattern/star/StarPattern1.java
@@ -0,0 +1,7 @@
+package dsa.pattern.star;
+
+public class StarPattern1 {
+	public static void main(String[] args) {
+		System.out.println("this is StarPattern1");
+	}
+}
